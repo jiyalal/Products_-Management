b9635de562ab65552e3887919986e875098f5a10
Bhushan Mahale <bhshnmhl@gmail.com>
2022-07-28 12:06:44 +0530

    add

---
+++ src/model/orderModel.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const orderModel = new mongoose.Schema({
+    userId: {type:ObjectId, ref:"User", required:true, unique:true},
+    items: [{
+      productId: {type:ObjectId, ref:"Products", required:true},
+      quantity: {type:Number, required:true}
+    }],
+    totalPrice: {type:Number, required:true},//comment: "Holds total price of all the items in the cart"
+    totalItems: {type:Number,required:true},// comment: "Holds total number of items in the cart"
+    totalQuantity: {type:Number,required:true},//, comment: "Holds total number of quantity in the cart"},
+    cancellable: {type:Boolean, default: true},
+    status: {type:String, default: 'pending', enum:[pending, completed, cancled]},
+    deletedAt: {type:Date},            // when the document is deleted
+    isDeleted: {type:Boolean, default: false},
+    createdAt: {type:Date},
+    updatedAt: {type:Date},
+  },{timestamps:true})
+
+  module.exports = mongoose.model("Order", orderModel);
--- src/routes/route.js
+++ src/routes/route.js
@@ -3,10 +3,29 @@ const router = express.Router()
 const userController = require("../controller/userController")
 const middleware=require("../Middleware/commonMid")
 
-
+//=====================[ USER API'S ]=========================
 router.post('/register', userController.createUser)
 router.post('/login', userController.loginUser)
 router.get('/user/:userId/profile',middleware.authenticate,middleware.authForParams, userController.getUserdata)
 router.put('/user/:userId/profile',userController.updateUser)
+
+//===================[ PRODUCT API'S]=========================
+router.post('/products') //rites
+router.get('/products') //jiya lal
+router.get('/products/:productId')// jiya lal
+router.put('/products/:productId') // bhushan
+router.delete('/products/:productId') // himashu
+
+//====================[ORDER API'S]============================
+
+router.post('/users/:userId/cart') //bhushan
+router.put('/users/:userId/cart')  // jiyalal
+router.get('/users/:userId/cart')  // himanshu
+router.delete('/users/:userId/cart') //ritesh
+
+//====================[CART API'S]=============================
+
+router.post('/users/:userId/orders')  //Bhushan
+router.put('/users/:userId/orders')  //Ritesh
 
 module.exports = router
