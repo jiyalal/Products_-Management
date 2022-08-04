97c5d27a6202d21c336c1ffaf4d30cd075c8fa48
maxxhimanshu <hsudhanshu31@gmail.com>
2022-08-04 12:05:58 +0530

    my com

--- src/controller/cartController.js
+++ src/controller/cartController.js
@@ -261,31 +261,21 @@ const getCart = async function (req, res) {
     try {
         const userId = req.params.userId
         if (userId.trim().length == 0)
-            return res.status(400).send({ status: false, Message: "userId Empty" })
+	      return res.status(400).send({ status: false, Message: "userId Empty" })
 
-        if (!mongoose.isValidObjectId(userId))
-            return res.status(400).send({ status: true, Message: "Invalid UserId !" })
+        if (!mongoose.isValidObjectId(userId)) 
+	      return res.status(400).send({ status: true, Message: "Invalid UserId !" })
 
         const checkCart = await cartModel.findOne({ userId: userId })
 
-        if (!checkCart)
-            return res.status(404).send({ status: false, Message: 'Cart not found' })
-        if (checkCart.items.length == 0)
-            return res.status(400).send({ status: false, Message: "Cart is empty" })
+        if (!checkCart) 
+        return res.status(404).send({ status: false, Message: 'Cart not found' })
+        if (checkCart.items.length==0)
+        return res.status(400).send({ status: false, Message: "Cart is empty" })
 
-        if (cart.userId != userId) return res.status(401).send({ status: false, message: "userId of cart not matched with user,unauthorized" })
-
-        if (cart.items.length == 0 && cart.totalPrice == 0 && cart.totalItems == 0) return res.status(400).send({ status: false, message: "cart is already deleted" })
-
-
-        let deletedcart = await cartModel.findOneAndUpdate({ userId: req.params.userId }, { items: [], totalItems: 0, totalPrice: 0 }, { new: true })
-
-        return res.status(204).send({ status: true, message: "Cart deleted successfull", data: deletedcart })
-
-    } catch (err)
-    {
-        return res.status(500).send({ status: false, message: err.message })
-    }
+         return res.status(200).send({ status: true, Message: 'Succcess', data: checkCart })
+    } catch (error) { 
+      res.status(500).send({ status: false, Message: error.message }) }
 }
 
 
